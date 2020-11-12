5fd15b2fce5ec65efd0192c95bc2ab25b3176843
saurabhnikam1997 <nikam280197@gmail.com>
2020-11-12 21:26:14 +0530

    Game played with single player startpositin and endposition

--- src/snakeladdergame/SnakeandLadder.java
+++ src/snakeladdergame/SnakeandLadder.java
@@ -1,8 +1,23 @@
 package snakeladdergame;
 
 public class SnakeandLadder {
+	
+	static int[] board = new int[101];
+	int Startposition = 0;
+	int Endposition = 100;
+	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
+		new SnakeandLadder();
+	//	SnakeandLadder.board();
+		SnakeandLadder.showboard(board);
+		
 	}
 
+
+	private static void showboard(int[] board) {
+		System.out.println("Snake and Ladder Game Board");
+		
+	}
 }
