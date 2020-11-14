63d1b1b466f7a595ac74d604b79bdc26d4d5fc17
saurabhnikam1997 <nikam280197@gmail.com>
2020-11-14 06:10:50 +0530

    play game with two player

--- src/snakeladdergame/SnakeandLadder.java
+++ src/snakeladdergame/SnakeandLadder.java
@@ -1,58 +1,73 @@
 package snakeladdergame;
 
 public class SnakeandLadder {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game");
-		//int[] board = new int[101];
-		new SnakeandLadder();
-		SnakeandLadder.rollDice();
-		
-	}
-	public static void rollDice() {
-		int position = 0;
-		int no_rolls = 0;
-		while(position==100) {
-			int Dice_value = (int)Math.floor((Math.random()*10 +1 )% 6);
-			System.out.println("No_of_die_roll"+no_rolls+"Position ofthe player"+position);
-			no_rolls=no_rolls+1;
-			int option = (int)Math.floor((Math.random()*10 +1 )% 3);
+	
+	private static int position_of_player1=0;
+	private static int position_of_player2=0;
+	private static int no_of_die_rolls=0;
+	private static int die_value;
+	private  static int values=0;
+	private static int play(int value) 
+	 {
+		values=value;
+		if(values<100) 
+		{
+			die_value=die_roll();
+			no_of_die_rolls=no_of_die_rolls+1;
+			int option=(int)Math.floor((Math.random()*10)% 3);
 			switch(option) 
 			{
-				case 0:
-					System.out.println("Nopaly");			
+			case 0:
 					break;
-			//Ladder
-				case 1:
-					if(position+Dice_value<=100)
-					{
-						position = position + Dice_value;
-						break;
-					}
-					else
-					{
-						position=0;
-						break;
-					}
-			//snake
-				case 2:
-						if(position>0 )
-						{
-							position = position - Dice_value;
-							System.out.println(position);
-							break;
-						}
-						else
-						{	
-							position=0;
-							break;
-						}
-				default:
-					System.out.println("Invalid Operation");;
-					break;	
+			case 1: 
+					ladder();
+					break;
+			case 2:  
+					snake();
+					break;
+			default:
+					System.out.println("Something unexpected happen: ");
 			}
 		}
-		System.out.println("number of die roll "+no_rolls+" position of player:"+position);
-
+		return values;
+	 }
+	private static void snake() 
+	{
+		if(values>die_value && values>0) 
+			values=values-die_value;
+		else
+			values=0;
+	}
+	private static int die_roll() 
+	{
+		die_value=(int)Math.floor((Math.random()*10 +1 )% 6);
+		return die_value;
+	}
+	private static void ladder()
+	{	
+		if(values+die_value<100) 
+		{
+			values=values+die_value;
+			if(values<100)
+			{
+				die_value=die_roll();
+				if(values+die_value<=100)
+				values=values+die_value;
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		while(position_of_player1<100 && position_of_player2<100) 
+		{
+			position_of_player1=play(position_of_player1);
+			System.out.println("number of die roll "+no_of_die_rolls+" position of player1: "+position_of_player1);
+			position_of_player2=play(position_of_player2);	
+			System.out.println("number of die roll "+no_of_die_rolls+" position of player2: "+position_of_player2);
+		}
+		if(position_of_player1==100)
+			System.out.println("player 1 is winner");
+		else
+			System.out.println("player2 is winner");
 	}
 }
